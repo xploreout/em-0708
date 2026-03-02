0a56e402cd9ed9db566dde72d32d242d23b8df28
TanLui <effishing@gmail.com>
2026-03-02 12:14:54 -0500

    route changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Announcements from './components/Announcements';
