f254790370b8e4b96464403329d4da601de08147
TanLui <effishing@gmail.com>
2026-03-04 20:12:02 -0500

    css upd

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -112,7 +112,7 @@ const Resources = () => {
   return (
     <section
       id='resources'
-      className='py-20 bg-gray-100 '
+      className='py-20 bg-gray-200 '
     >
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         {/* Header */}
